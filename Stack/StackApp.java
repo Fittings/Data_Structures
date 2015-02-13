9073d5d2b29b584540704343e0ac92fa620ff254
Cameron Milsom <cameron.live@gmail.com>
2015-02-13 13:17:59 +1300

    small update to app file

--- Stack/StackApp.java
+++ Stack/StackApp.java
@@ -3,8 +3,8 @@ public class StackApp {
 
 	public static void main(String[] args) {
 		Stack myStack = new Stack();
-		myStack.push("Hello");
-		myStack.push("World");
+		myStack.push("My");
+		myStack.push("Name");
 		System.err.println(myStack.pop());
 		System.err.println(myStack.pop());
 		System.err.println(myStack.pop());
@@ -14,10 +14,10 @@ public class StackApp {
 		System.err.println(myStack.pop());
 		System.err.println(myStack.pop());
 		System.err.println(myStack.pop());
-		myStack.push("Hello");
-		myStack.push("World");
-		myStack.push("Hello");
-		myStack.push("World");
+		myStack.push("is");
+		myStack.push("Cameron");
+		myStack.push("Darren");
+		myStack.push("Milsom");
 		System.err.println(myStack.pop());
 		System.err.println(myStack.pop());
 		System.err.println(myStack.pop());
